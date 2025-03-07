754b442581287c9847c8df877eaf28f88a0c29a4
Muhammad Azam <161217248+azam-izm@users.noreply.github.com>
2025-03-08 00:04:54 +0500

    Update extreme_value_finder_three_levels.dart

--- Dart Tutorials/extreme_value_finder_three_levels.dart
+++ Dart Tutorials/extreme_value_finder_three_levels.dart
@@ -18,61 +18,67 @@ void main() {
   final numbers = [-100, 20, -40, 30, 50, 30, 60, -70, 55];
 
   // ===== Reduce Method =====
-  final largest = numbers.reduce(max); 
+  final largest = numbers.reduce(max);
   final smallest = numbers.reduce(min);
-  //OR 
-  int largestNumber = numbers.reduce((a, b) => a > b ? a : b); print('(Reduce): Largest Number is $largestNumber');
-  int smallestNumber = numbers.reduce((a, b) => a < b ? a : b); print('(Reduce): Smallest Number is $smallestNumber\n');
-  
+  //OR
+  int largestNumber = numbers.reduce((a, b) => a > b ? a : b);
+  print('(Reduce): Largest Number is $largestNumber'); //(Reduce): Largest Number is 60
+  int smallestNumber = numbers.reduce((a, b) => a < b ? a : b);
+  print('(Reduce): Smallest Number is $smallestNumber\n'); //(Reduce): Smallest Number is -100
+
   final secondLargest = numbers.where((n) => n < largest).reduce(max);
   final thirdLargest = numbers.where((n) => n < secondLargest).reduce(max);
-  
+
   final secondSmallest = numbers.where((n) => n > smallest).reduce(min);
   final thirdSmallest = numbers.where((n) => n > secondSmallest).reduce(min);
-  
-  print('(Reduce) Largest: $largest, Second: $secondLargest, Third: $thirdLargest');
-  print('(Reduce) Smallest: $smallest, Second: $secondSmallest, Third: $thirdSmallest\n');
 
- // ===== Loop Method =====
-int loopLargest = numbers[0];
-int loopSecondLargest = numbers[0];
-int loopThirdLargest = numbers[0];
-int loopSmallest = numbers[0];
-int loopSecondSmallest = (0); 
-int loopThirdSmallest = (0);  
+  print('(Reduce) Largest: $largest, Second: $secondLargest, Third: $thirdLargest'); //(Reduce) Largest: 60, Second: 55, Third: 50
+  print('(Reduce) Smallest: $smallest, Second: $secondSmallest, Third: $thirdSmallest\n'); //(Reduce) Smallest: -100, Second: -70, Third: -40
 
-for (final num in numbers) {
-  // Update largest values 
-  if (num > loopLargest) {
-    loopThirdLargest = loopSecondLargest;
-    loopSecondLargest = loopLargest;
-    loopLargest = num;
-  } else if (num > loopSecondLargest && num != loopLargest) {
-    loopThirdLargest = loopSecondLargest;
-    loopSecondLargest = num;
-  } else if (num > loopThirdLargest && num != loopSecondLargest && num != loopLargest) {
-    loopThirdLargest = num;
+  // ===== Loop Method =====
+  int loopLargest = numbers[0];
+  int loopSecondLargest = numbers[1];
+  int loopThirdLargest = numbers[2];
+  int loopSmallest = numbers[3];
+  int loopSecondSmallest = numbers[4];
+  int loopThirdSmallest = numbers[5];
+
+  for (final num in numbers) {
+    // Update largest values
+    if (num > loopLargest) {
+      loopThirdLargest = loopSecondLargest;
+      loopSecondLargest = loopLargest;
+      loopLargest = num;
+    } else if (num > loopSecondLargest && num != loopLargest) {
+      loopThirdLargest = loopSecondLargest;
+      loopSecondLargest = num;
+    } else if (num > loopThirdLargest &&
+        num != loopSecondLargest &&
+        num != loopLargest) {
+      loopThirdLargest = num;
+    }
+
+    // Update smallest values
+    if (num < loopSmallest) {
+      loopThirdSmallest = loopSecondSmallest;
+      loopSecondSmallest = loopSmallest;
+      loopSmallest = num;
+    } else if (num < loopSecondSmallest && num != loopSmallest) {
+      loopThirdSmallest = loopSecondSmallest;
+      loopSecondSmallest = num;
+    } else if (num < loopThirdSmallest &&
+        num != loopSecondSmallest &&
+        num != loopSmallest) {
+      loopThirdSmallest = num;
+    }
   }
-
-  // Update smallest values
-  if (num < loopSmallest) {
-    loopThirdSmallest = loopSecondSmallest;
-    loopSecondSmallest = loopSmallest;
-    loopSmallest = num;
-  } else if (num < loopSecondSmallest && num != loopSmallest) {
-    loopThirdSmallest = loopSecondSmallest;
-    loopSecondSmallest = num;
-  } else if (num < loopThirdSmallest && num != loopSecondSmallest && num != loopSmallest) {
-    loopThirdSmallest = num;
-  }
-}
-  print('(Loop) Largest: $loopLargest, Second: $loopSecondLargest, Third: $loopThirdLargest');
-  print('(Loop) Smallest: $loopSmallest, Second: $loopSecondSmallest, Third: $loopThirdSmallest\n');
+  print('(Loop) Largest: $loopLargest, Second: $loopSecondLargest, Third: $loopThirdLargest'); //(Loop) Largest: 60, Second: 55, Third: 50
+  print('(Loop) Smallest: $loopSmallest, Second: $loopSecondSmallest, Third: $loopThirdSmallest\n'); //(Loop) Smallest: -100, Second: -70, Third: -40
 
   // ===== Sorting Method =====
   final sorted = List<int>.from(numbers)..sort();
-  print('(Sorting) Largest: ${sorted.last}, Second: ${sorted[sorted.length - 2]}, Third: ${sorted[sorted.length - 3]}');
-  print('(Sorting) Smallest: ${sorted.first}, Second: ${sorted[1]}, Third: ${sorted[2]}\n');
+  print('(Sorting) Largest: ${sorted.last}, Second: ${sorted[sorted.length - 2]}, Third: ${sorted[sorted.length - 3]}'); //(Sorting) Largest: 60, Second: 55, Third: 50
+  print('(Sorting) Smallest: ${sorted.first}, Second: ${sorted[1]}, Third: ${sorted[2]}\n'); //(Sorting) Smallest: -100, Second: -70, Third: -40
 
   // ===== Removal Method =====
   var temp = List<int>.from(numbers);
@@ -89,6 +95,6 @@ for (final num in numbers) {
   temp.remove(removalSecondSmallest);
   final removalThirdSmallest = temp.reduce(min);
 
-  print('(Removal) Largest: $removalLargest, Second: $removalSecondLargest, Third: $removalThirdLargest');
-  print('(Removal) Smallest: $removalSmallest, Second: $removalSecondSmallest, Third: $removalThirdSmallest');
+  print('(Removal) Largest: $removalLargest, Second: $removalSecondLargest, Third: $removalThirdLargest'); //(Removal) Largest: 60, Second: 55, Third: 50
+  print('(Removal) Smallest: $removalSmallest, Second: $removalSecondSmallest, Third: $removalThirdSmallest'); //(Removal) Smallest: -100, Second: -70, Third: -40
 }
